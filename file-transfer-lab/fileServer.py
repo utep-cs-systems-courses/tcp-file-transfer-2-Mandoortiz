4720fd91ff3f0537415746f63c52e4f1d5a78bba
Armando Ortiz <mandoortiz6209@gmail.com>
2020-10-18 19:12:28 -0600

    added forking to fileClient.py

--- file-transfer-lab/fileServer.py
+++ file-transfer-lab/fileServer.py
@@ -2,7 +2,7 @@
 
 import sys
 sys.path.append("../lib")       # for params
-import re, socket, params
+import re, socket, params, os
 from framedSock import framedSend, framedReceive
 
 switchesVarDefaults = (
@@ -24,22 +24,30 @@ bindAddr = ("127.0.0.1", listenPort)
 lsock.bind(bindAddr)
 lsock.listen(5)
 print("listening on:", bindAddr)
-
-sock, addr = lsock.accept()
-
-print("connection rec'd from", addr)
-
-
-
-
 while True:
-    payload = framedReceive(sock, debug)
-    if debug: print("rec'd: ", payload)
-    if not payload:
-        break
-    payload = payload.decode()
-    filename = payload.split("%^&")[0]
-    filecontents = payload.split("%^&")[1]
-    file = open("received"+filename,"w")
-    file.write(filecontents)
-    file.close()
+    try:
+        sock, addr = lsock.accept()
+    except:
+        print("Error: failure to transfer file")
+        sys.exit(1)
+        
+    print("connection rec'd from", addr)
+
+    if not os.fork():
+        payload = framedReceive(sock, debug)
+        if debug: print("rec'd: ", payload)
+        
+        if payload is None:
+            print("Error: file is empty")
+            sys.exit(1)
+        payload = payload.decode()
+        file_name = payload.split("%^&")[0]
+        file_contents = payload.split("%^&")[1]
+
+        if(os.path.isfile(file_name)):
+            print("file with name already exists.")
+        else:
+            print("Receiving "  +file_name)
+            file_rec = open(file_name,"w")
+            file_rec.write(file_contents)
+            file_rec.close()
